0ba610d9d10813a30222cce8dfd213290c35ec5f
sahil panchasara <spanchasara@argusoft.com>
2023-07-11 10:53:49 +0530

    Added user endpoints

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import catchAsync from "../utils/general/catch-async.js";
+import * as userService from "../services/user.service.js";
+
+const getAllUsers = catchAsync(async (req, res) => {
+  const allUsers = await userService.getAllUsers();
+  res.send(allUsers);
+});
+
+const getUserById = catchAsync(async (req, res) => {
+  const user = await userService.getUserById(req.loggedInUserId);
+  res.send(user);
+});
+
+const getAnyUserById = catchAsync(async (req, res) => {
+  const user = await userService.getAnyUserById(req.params.userId);
+  res.send(user);
+});
+
+const getUserByEmail = catchAsync(async (req, res) => {
+  const user = await userService.getUserByEmail(req.query.email);
+  res.send(user);
+});
+
+const updateUserById = catchAsync(async (req, res) => {
+  const user = await userService.updateUserById(req.loggedInUserId, req.body);
+  res.send(user);
+});
+
+const deleteUserById = catchAsync(async (req, res) => {
+  const allUsers = await userService.deleteUserById(req.loggedInUserId);
+  res.send(allUsers);
+});
+
+const getUsersProject = catchAsync(async (req, res) => {
+  const users = await userService.getUsersProject(req.loggedInUserId);
+  res.send(users);
+});
+
+export {
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  getUsersProject,
+  getAnyUserById,
+  getUserByEmail,
+};
---
+++ Backend/src/services/user.service.js
@@ -0,0 +1,67 @@
+import { projectUserModel, userModel } from "../models/index.js";
+import ApiError from "../utils/api-error/index.js";
+
+const getUserById = async (userId) => {
+  const user = await userModel.findById(userId, { password: 0 });
+  if (!user) throw new ApiError(404, "User not found");
+  return user;
+};
+
+const getAnyUserById = async (userId) => {
+  const user = await userModel.findById(userId, { password: 0 });
+  if (!user) throw new ApiError(404, "User not found");
+  return user;
+};
+
+const createUser = async (body) => {
+  const newUser = await userModel.create(body);
+  if (!newUser) throw new ApiError(500, "Internal Server error");
+  return newUser;
+};
+
+const getAllUsers = async () => {
+  const users = await userModel.find({}, { password: 0 });
+  if (!users) throw new ApiError(404, "Internal Server error");
+  return users;
+};
+
+const updateUserById = async (userId, body) => {
+  const user = await userModel.findByIdAndUpdate(userId, body);
+  if (!user) throw new ApiError(404, "User not found");
+  return user;
+};
+
+const deleteUserById = async (userId) => {
+  const user = await userModel.findByIdAndDelete(userId);
+  if (!user) throw new ApiError(404, "User not found");
+  return user;
+};
+
+const getUserByEmail = async (email, wantPassword = false) => {
+  const user = await userModel.findOne(
+    { email },
+    !wantPassword && { password: 0 }
+  );
+  return user;
+};
+
+// get all projects of user
+const getUsersProject = async (userId) => {
+  const projects = await projectUserModel.find({ user: userId });
+  if (!projects) {
+    throw new ApiError(404, "No Projects Found");
+  }
+
+  return projects;
+};
+
+export {
+  getUserById,
+  createUser,
+  updateUserById,
+  getAllUsers,
+  deleteUserById,
+  getUserByEmail,
+  getUsersProject,
+  getAnyUserById,
+};
